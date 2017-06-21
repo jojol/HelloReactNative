b4b3d37921f65ffeceaf39ac1b3587e1e14baf1f
周继洪 <zhoujihong19@gmail.com>
2017-06-21 12:24:06 +0800

    upload project
    upload project

---
+++ index.android.js
@@ -0,0 +1,86 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+} from 'react-native'
+
+export default class HelloWord extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native! jojol
+        </Text>
+        <Text style={styles.instructions}>
+          jojol To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+        <Image 
+          style={{
+            width: 300,
+            height: 200,
+          }}
+          resizeMode={"contain"}
+          source={{uri:'https://unsplash.it/600/400/?random'}}
+        />
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+        <Image source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}} style={{width: 193, height: 110}} />
+        <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+    backgroundColor: 'powderblue',
+  },
+  red: {
+    color: 'red',
+  },
+});
+
+AppRegistry.registerComponent('HelloWord', () => HelloWord);
