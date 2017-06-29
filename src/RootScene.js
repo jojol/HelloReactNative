4bbbf92a9efae325ef49a9d8187819b1ef4cf528
周继洪 <zhoujihong19@gmail.com>
2017-06-29 11:48:13 +0800

    Add ImageItem to test Image

--- src/RootScene.js
+++ src/RootScene.js
@@ -5,40 +5,83 @@ import React, { PureComponent } from 'react'
 import {
   AppRegistry,
   StyleSheet,
+  ScrollView,
   Text,
+  TextInput,
   Image,
   View,
   Dimensions,
 } from 'react-native'
 
+import ImageItem from './widget/ImageItem'
 
 // create a component
 class RootScene extends PureComponent {
+
+    props: {
+        onSubmitEditing: Function,
+        text: string,
+        onChangeText: Function,
+        onSubmit: Function,
+      }
+
+      state: {
+        text: string
+      }
     // constructor() {
     //     super()
     //
     //     StatusBar.setBarStyle('light-content')
     // }
+      constructor(props) {
+          super(props);
+          this.state = { text: 'Useless Placeholder' };
+          // console.log(`ListRequest - Success node:${this.requestNode}`);
+      }
+
+    onChangeText(text: string) {
+        this.setState({ text: text });
+        console.log(`onChangeText :${this.state.text} vs ${text}`);
+        // this.props.onChangeText && this.props.onChangeText()
+    }
+
+    onSubmitEditing() {
+        console.log(`onSubmitEditing :${this.state.text}`);
+        if (this.props.onSubmit) {
+            this.props.onSubmit(this.state.text);
+        }
+    }
+
+    // onChangeText={(text) => { this.onChangeText(text) }}
+    onSubmit() {
+        console.log(`onSubmit :${this.state.text}`);
+    }
 
     render() {
         return (
-            <View style={{height:400}}>
+          <ScrollView>
+            <View style={{height:900}}>
               <Text style={styles.instructions}>
-                 当前屏幕宽度: {Dimensions.get('window').width}
+                 当前屏幕宽度: {Dimensions.get('window').width} input: {this.state.text}
               </Text>
-              {/* <View style={{
-                flex: 1,
-                flexDirection: 'column',
-                backgroundColor:'orange',
-                justifyContent: 'flex-start',
-                alignItems: 'center'
-              }}>
-                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-              </View> */}
 
+              <ImageItem />
 
+              {/*
+              <Image source={{uri: 'http://jojol.imwork.net/zb_users/upload/2017/05/201705131494607219270064.jpg'}}
+                style={{width: 400, height: 400,backgroundColor: 'red'}} /> */}
+
+              <TextInput
+                style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                onSubmitEditing={this.onSubmitEditing.bind(this)}
+                onChangeText={(text) => {
+                                text = text.replace(/ /g, '_');
+                                console.log(`onChangeText replace :${this.state.text} vs ${text}`);
+                                this.setState({text});
+                              }}
+                returnKeyType='next'
+                value={this.state.text}
+              />
 
               <View style={{flex: 1, backgroundColor: 'powderblue'}} />
               <View style={{flex: 2, backgroundColor: 'skyblue'}} />
@@ -48,6 +91,39 @@ class RootScene extends PureComponent {
                 <View style={{flex: 1, backgroundColor: 'yellow'}}></View>
               </View>
             </View>
+
+
+            <View style={{height:400}}>
+              <Text style={styles.instructions}>
+                 当前屏幕宽度: {Dimensions.get('window').width} input: {this.state.text}
+              </Text>
+              <TextInput
+                style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                onSubmitEditing={this.onSubmitEditing.bind(this)}
+                onChangeText={(text) => {
+                                text = text.replace(/ /g, '_');
+                                console.log(`onChangeText replace :${this.state.text} vs ${text}`);
+                                this.setState({text});
+                              }}
+                returnKeyType='next'
+                value={this.state.text}
+              />
+
+
+              <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                backgroundColor:'orange',
+                justifyContent: 'flex-start',
+                alignItems: 'center'
+              }}>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+              </View>
+            </View>
+          </ScrollView>
+
         );
     }
 }
---
+++ src/widget/ImageItem.js
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2017-present, Liu Jinyong
+ * All rights reserved.
+ *
+ * https://github.com/huanxsd/MeiTuan
+ * @flow
+ */
+
+//import liraries
+import React, { PureComponent, PropTypes } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TextInput,
+  Image,
+  View,
+  Dimensions,
+} from 'react-native'
+
+// create a component
+class ImageItem extends PureComponent {
+
+
+  props: {
+      onSubmitEditing: Function
+    }
+
+    state: {
+      error: string,
+      loading: string,
+      progress: number
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: "none",
+            loading: "none",
+            progress: 0
+          };
+        // console.log(`ListRequest - Success node:${this.requestNode}`);
+    }
+
+    onSubmitEditing() {
+        console.log(`onSubmitEditing :${this.state.loading}`);
+    }
+
+    render() {
+        return (
+          <Image
+            source={{uri: 'https://seagm.github.io/react-test/images/ucp-cover.png'}}
+            resizeMode={'stretch'}
+            style={{width: 400, height: 400,backgroundColor: 'red',borderRadius:70,borderWidth:2 }}
+            onLoadStart={(e) => {
+              this.setState({loading: 'started'});
+              console.log('Image onLoadStart');
+            }}
+            onError={(e) => {
+              this.setState({error: e.nativeEvent.error, loading: 'failed'});
+              console.log(`Image onError :${e.nativeEvent.error}`);
+            }}
+            onProgress={(e) => {
+              this.setState({progress: Math.round(100 * e.nativeEvent.loaded / e.nativeEvent.total)});
+              console.log(`Image progress :${this.state.progress}`);
+            }}
+            onLoad={() => this.setState({loading: 'success', error: 'no error'})}>
+                <View style={{flex: 1,backgroundColor: 'transparent'}}>
+                  <Text>{this.state.progress}%</Text>
+                    <Text>图片加载error: {this.state.error} loading: {this.state.loading}</Text>
+                </View>
+          </Image>
+        );
+    }
+}
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+    backgroundColor: 'powderblue',
+  },
+  red: {
+    color: 'red',
+  },
+});
+//make this component available to the app
+export default ImageItem;
